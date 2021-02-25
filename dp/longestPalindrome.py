6067cd1fb1636f7c1273499b41eca181628ab3e9
ToulonDu <seaduhe@sina.com>
2021-02-26 00:04:04 +0800

    feat: 5. 最长回文子串

---
+++ dp/longestPalindrome.py
@@ -0,0 +1,49 @@
+'''
+5. 最长回文子串
+给你一个字符串 s，找到 s 中最长的回文子串。
+
+ 
+
+示例 1：
+
+输入：s = "babad"
+输出："bab"
+解释："aba" 同样是符合题意的答案。
+示例 2：
+
+输入：s = "cbbd"
+输出："bb"
+示例 3：
+
+输入：s = "a"
+输出："a"
+示例 4：
+
+输入：s = "ac"
+输出："a"
+ 
+
+提示：
+
+1 <= s.length <= 1000
+s 仅由数字和英文字母（大写和/或小写）组成
+'''
+# 比较简单，以每个字母和字母对为起始向两边扩展，找出所有的回文
+# 应该也可以用动态规划来解
+class Solution:
+    def expandAroundCenter(self, s, left, right):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return left + 1, right - 1
+
+    def longestPalindrome(self, s: str) -> str:
+        start, end = 0, 0
+        for i in range(len(s)):
+            left1, right1 = self.expandAroundCenter(s, i, i)
+            left2, right2 = self.expandAroundCenter(s, i, i + 1)
+            if right1 - left1 > end - start:
+                start, end = left1, right1
+            if right2 - left2 > end - start:
+                start, end = left2, right2
+        return s[start: end + 1]
