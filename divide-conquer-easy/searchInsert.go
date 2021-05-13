7ffdd1ed91e2a8e74228e66d233d97255bd3c88f
dulb <dulb@seeyon.com>
2021-05-13 18:55:00 +0800

    feat: problem-35. 搜索插入位置

---
+++ divide-conquer-easy/searchInsert.go
@@ -0,0 +1,46 @@
+// 35. 搜索插入位置
+// 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+
+// 你可以假设数组中无重复元素。
+
+// 示例 1:
+
+// 输入: [1,3,5,6], 5
+// 输出: 2
+// 示例 2:
+
+// 输入: [1,3,5,6], 2
+// 输出: 1
+// 示例 3:
+
+// 输入: [1,3,5,6], 7
+// 输出: 4
+// 示例 4:
+
+// 输入: [1,3,5,6], 0
+// 输出: 0
+package main
+
+import "fmt"
+
+func searchInsert(nums []int, target int) int {
+	var start int
+	end := len(nums) - 1
+
+	for start <= end {
+		mid := (end-start)/2 + start
+		if nums[mid] > target {
+			end = mid - 1
+		} else if nums[mid] < target {
+			start = mid + 1
+		} else {
+			return mid
+		}
+	}
+	return start
+}
+
+func main() {
+	arr := []int{1, 3, 5, 6}
+	fmt.Println(searchInsert(arr, 6))
+}
